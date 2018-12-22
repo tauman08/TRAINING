9a3966a4680e8fec7121e7318d968cc8a101d536
a.timofeev <tauman08@gmail.com>
2018-12-22 16:16:38 +0300

    выполнено дз

---
+++ Exercise5/dz5-1/src/js/script.js
@@ -0,0 +1,10 @@
+let btnStart    = document.getElementById('start'),
+elValue         = document.querySelectorAll('.result-table>div[class$=-"value"]'),
+elInput         = document.querySelectorAll('.expenses-item');
+
+
+let btnApprow = document.getElementsByClassName('expenses-item-btn')[0],
+btnCalc = document.getElementsByClassName('count-budget-btn');
+
+let elOpt = document.querySelectorAll('.optionalexpenses-item');
+let elAll = document.querySelectorAll('.choose-income, #savings, #sum, #percent, .year-value, month-value, .day-value');
